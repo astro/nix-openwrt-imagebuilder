2ddc9f041b401172ebdc1ea0559d03a9e5b74019
Astro <astro@spaceboyz.net>
2022-04-28 23:03:21 +0200

    hashes/22.03.0-rc1: add

---
+++ hashes/22.03.0-rc1.nix
@@ -0,0 +1,279 @@
+{
+  "apm821xx"."nand" = {
+    sha256 = "0k113fnxfbhicr5xy23d20lcgwhsdcqygq2z2kc1n7g5796aghxv";
+    feedsSha256.base = "0n6zs5gyqpvyim909jhacgk6cpf673jx7hlwh2wz0ycakn1hysvy";
+  };
+  "apm821xx"."sata" = {
+    sha256 = "0p6xx4ivf256l8p6n9vxb807lr52lbldhihvdflfqg5h928dx7l6";
+    feedsSha256.base = "0n6zs5gyqpvyim909jhacgk6cpf673jx7hlwh2wz0ycakn1hysvy";
+  };
+  "archs38"."generic" = {
+    sha256 = "1lmf69k9xsh0mgmybsgzx09v0psv6lrqqd5j1rxcndilqkqbqnjb";
+    feedsSha256.base = "0890px0m9ihn3phffbq2lqjgw0amwinvmh2d4mc3rncbwa5df0n5";
+  };
+  "at91"."sama7" = {
+    sha256 = "0aa4ql5s6mhg0zb0ngpqrbdcdi7grfh6xrw2rr7fgadcvn41a484";
+    feedsSha256.base = "1d4iacbvp7mcxgw5dipvdd27wqzqfcbb6kp286fxllqfzlsqzggd";
+  };
+  "at91"."sama5" = {
+    sha256 = "0f43x128569da69y4bxgqs06s8xyi7brbxzjf8p2b83z2kpv35vs";
+    feedsSha256.base = "05s25vpsp692y4m1j2b4w5ix0wiyacysm9byk1yxb7h25y02m9vc";
+  };
+  "at91"."sam9x" = {
+    sha256 = "098rxgqdj7mlim5zkimg6lypxdrj35a97ar1g0drl78kyrmzl1jc";
+    feedsSha256.base = "0br9v3b05l25xbl4d7870fwagy5dqq2ph2pcalbp6cc6s5ff69fm";
+  };
+  "ath25"."generic" = {
+    sha256 = "18sdvr38zml9l8kbvp67ymiz7fbr5x4jb8cshiq2yjbbp8d679sg";
+    feedsSha256.base = "1nx5546wgfk69pyvgaszbrcvmjymmcy1znnxjk4dxfkdqv15a25g";
+  };
+  "ath79"."generic" = {
+    sha256 = "1w2j5cq2i8v48vi2waqjybm83zz6ic2kgnz0l20mjbw3nqkmk9dq";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "ath79"."mikrotik" = {
+    sha256 = "0y46i1daxzv88b17rhv5yyf00mljk9qa8grr86nyz9gisdq19a5p";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "ath79"."nand" = {
+    sha256 = "0vbxs3h0ay4xsmzwv6fy2vl8chw8xj494p85iyzibj73yajdbv1x";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "ath79"."tiny" = {
+    sha256 = "0g3lj9s01k7d0hha2ym1k5djsfkq9wlgc4gwfh20ya8wx9dn5inl";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "bcm27xx"."bcm2708" = {
+    sha256 = "0ml3fjwpxdc6f1zg3xngn7rjgihddr4j9p2c46afphd3g2bn0yfa";
+    feedsSha256.base = "0b6r8y6gaynysc437x0m35dflzxxk6pi4ydzl74wmvjnjk7w33ph";
+  };
+  "bcm27xx"."bcm2709" = {
+    sha256 = "0q7rlc39b92skdblikprx0b99b249v65xl7c6plnpip2vibx6fpy";
+    feedsSha256.base = "0nd4irbbq8jm8ivd2h1swhn7j22i211z8y6xm0ywa65n6pl9qc85";
+  };
+  "bcm27xx"."bcm2710" = {
+    sha256 = "06vxwdalcxx7k1hp1rb67k2inhxayyif36hwzq38hwx25zb5899g";
+    feedsSha256.base = "0bgfrgw92852j6yzbblkyw7w7j8rqhmnk844y2b7l8400m9ncci2";
+  };
+  "bcm27xx"."bcm2711" = {
+    sha256 = "1kx2zsjgn4g0gqxlg2x3xsjbi4krcyz62cglmyf54f3598sr9yxh";
+    feedsSha256.base = "13ll513j7basdlf4j0bqfsskf33jfvvmbhzhm0h8qq1cpnars597";
+  };
+  "bcm47xx"."generic" = {
+    sha256 = "0shsgq5fh7fik4czq20i53j3pszmn2b4jfc1ivsk0i16328r7b61";
+    feedsSha256.base = "1qb43kcnrblkxg85pm16sn80f157s997l1xiv8fsfhmxksiplnlr";
+  };
+  "bcm47xx"."mips74k" = {
+    sha256 = "0dqzm9jmv3zgaq5kdwimb5cv2zism6zacwxxsd4dg5s339x5c226";
+    feedsSha256.base = "1p31ig4r6frg7wn9z3dfsr3mz2hg6a8633k79nipcpf1jf060flg";
+  };
+  "bcm47xx"."legacy" = {
+    sha256 = "13ji1kqf0rsxnjpkmmw77lg4vc7xsj0wajq43clfdvlmx2k9nfx0";
+    feedsSha256.base = "1qb43kcnrblkxg85pm16sn80f157s997l1xiv8fsfhmxksiplnlr";
+  };
+  "bcm4908"."generic" = {
+    sha256 = "0px27rf592qcb9yb18szc7s5wk6k4w3yxnr13xaw3092zrssz8s9";
+    feedsSha256.base = "0bgfrgw92852j6yzbblkyw7w7j8rqhmnk844y2b7l8400m9ncci2";
+  };
+  "bcm53xx"."generic" = {
+    sha256 = "02mr2b0wwlp7mclpf44iv2rx9kmg6yidyfw8hnkvnc2kzs0gg189";
+    feedsSha256.base = "0bkf4ppwd399rqidy166zag6mi6ixqzivl7khr5v3kfh9b1dycgl";
+  };
+  "bcm63xx"."generic" = {
+    sha256 = "198y0inn6fr3zv42x6c72iw1yqbr9casymvm9n3n8fjb0jrkygl5";
+    feedsSha256.base = "1nx5546wgfk69pyvgaszbrcvmjymmcy1znnxjk4dxfkdqv15a25g";
+  };
+  "bcm63xx"."smp" = {
+    sha256 = "1n35b6h4wwd1nqr3fpihp6lbsmn2b2j81pfnmv2ap92k0pswj490";
+    feedsSha256.base = "1nx5546wgfk69pyvgaszbrcvmjymmcy1znnxjk4dxfkdqv15a25g";
+  };
+  "gemini"."generic" = {
+    sha256 = "0gr2yr4cvd7hwshh84w3wdgjxd9sxngak8d9pajkwc1snl58i8jk";
+    feedsSha256.base = "1jcv5918ib00qkjiw2mh9grcpkv66y8d5wl6gmxzp1l58gzlqhyz";
+  };
+  "imx"."cortexa7" = {
+    sha256 = "034cyn5ix6z1y7c6dq5qb372sh649l03ik1ga93kdm90vwrkqx4m";
+  };
+  "imx"."cortexa9" = {
+    sha256 = "085cdg8jn8r8mj280jv93l8wsg9q08yql4jb8rfw04f93nnicjcy";
+    feedsSha256.base = "0wlh5gg014zyc1i52ydxj7g87hz9ijzvvk7aq0pamy8wird2pv2x";
+  };
+  "ipq40xx"."generic" = {
+    sha256 = "00j8m03py9kl9r9swsnqjl9d6cnhpd2gpxb0y4ryc5waaa6cvjfp";
+    feedsSha256.base = "0nd4irbbq8jm8ivd2h1swhn7j22i211z8y6xm0ywa65n6pl9qc85";
+  };
+  "ipq40xx"."mikrotik" = {
+    sha256 = "0asckyfxyl03g5a75clb085f0zw019cciiygw6x7mz6cihgvwyaw";
+    feedsSha256.base = "0nd4irbbq8jm8ivd2h1swhn7j22i211z8y6xm0ywa65n6pl9qc85";
+  };
+  "ipq806x"."generic" = {
+    sha256 = "12anchaf1c287wwsr5k1ds61mxgpy59f9n1y1dakky42ip7zyaa7";
+    feedsSha256.base = "11mlcglh396x0p0sfp3hvy8wb7ng8jbrzgkm45qijnk66rkyzdpq";
+  };
+  "kirkwood"."generic" = {
+    sha256 = "0x5qvibwjd9mhbw88zibdldjz15z4y2yc4gmbw97nb088m26b4n1";
+    feedsSha256.base = "0vgv92g0rqbi9sbdrnmcxnd7gqssdkd2fqi5n066w3fp773aaf04";
+  };
+  "lantiq"."xrx200" = {
+    sha256 = "0f62i8fxbip4a6bnda36rhanfhlkrg5r03vi9gsya2i0w8kwzrmd";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "lantiq"."xway" = {
+    sha256 = "0xqlb5ns7i18m4hc05yyy0jzd426s60l0ym0s8bn06jj226z5ns4";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "lantiq"."xway_legacy" = {
+    sha256 = "02337d9l03kmaks62zv355dxzn16d8i4m41xzn8wc70g77kwzwhm";
+  };
+  "lantiq"."ase" = {
+    sha256 = "1bwcpd8rwhb0yvbxw38ml4x9qwh42bxbkpvyj2kydvfhb6skv98y";
+  };
+  "layerscape"."armv8_64b" = {
+    sha256 = "0vb05kk5mrfmvvzhc2y9l3xjb81k4i4v9pp6p8cwj3dqhxfwxsdk";
+    feedsSha256.base = "1h3ail2bphhvd8vddarqna1s968zxfviqn8q112hjn9cxcmbx6q5";
+  };
+  "layerscape"."armv7" = {
+    sha256 = "01c6md6jm17nxv191cagxz8mgvayjjq6r03mdq42gvrmsx4w04l3";
+    feedsSha256.base = "0nd4irbbq8jm8ivd2h1swhn7j22i211z8y6xm0ywa65n6pl9qc85";
+  };
+  "malta"."be" = {
+    sha256 = "0cz2x38wv7m99p3ynd0fn4dvkdwdw57a4yjspc47cwwbz5v8sk8b";
+  };
+  "mediatek"."mt7622" = {
+    sha256 = "02j8gqjhdpiqi7zjw1ypnjj1wqd2hcvm6lbgbhy5av803xhwmdn0";
+    feedsSha256.base = "0bgfrgw92852j6yzbblkyw7w7j8rqhmnk844y2b7l8400m9ncci2";
+  };
+  "mediatek"."mt7623" = {
+    sha256 = "1xfby3icwfgc8bkrs2f98183kn2k95jlmhyn686r08vvhxnp3psd";
+    feedsSha256.base = "0nd4irbbq8jm8ivd2h1swhn7j22i211z8y6xm0ywa65n6pl9qc85";
+  };
+  "mediatek"."mt7629" = {
+    sha256 = "09ansys76laz3afhyh0c5h16c3skzdz3jdk75ka89rmndklwmd9s";
+    feedsSha256.base = "047jn0idgmxwssl56g1xxja3j3q80wn99qaxy4l7w7d8h10g8lw1";
+  };
+  "mpc85xx"."p1010" = {
+    sha256 = "1bv21xhifbk5vaxlg9xnx40sihbc0w93207m4rfx3fd0ayrbdqnz";
+    feedsSha256.base = "0sm48bygyxdwvkr96lhvx7ivrabkygkj2qy32cfs6y8idz4yxb7r";
+  };
+  "mpc85xx"."p1020" = {
+    sha256 = "16z2nzm39bbhqifvizjr461zz3n6c4x8f1wqmn6d4rby6f2m62ws";
+    feedsSha256.base = "0sm48bygyxdwvkr96lhvx7ivrabkygkj2qy32cfs6y8idz4yxb7r";
+  };
+  "mpc85xx"."p2020" = {
+    sha256 = "0zbl8kiy8dv6lrc8rwna9m9s22nb3iglmv9qgy0zfkvd9rm9xm3m";
+    feedsSha256.base = "0sm48bygyxdwvkr96lhvx7ivrabkygkj2qy32cfs6y8idz4yxb7r";
+  };
+  "mvebu"."cortexa9" = {
+    sha256 = "0vsjssnfc4ak9vyyigkbsgd16d0pa5gfglwh1ssbgmkz2r3c34jy";
+    feedsSha256.base = "0za06nwg5i0xb810vk4plg97lfhb63k8707v58q36qg96zvhfkpc";
+  };
+  "mvebu"."cortexa53" = {
+    sha256 = "0yc969q3qcc30ibpahb5l7fpmb03fd16gvfrqsmc9g6g8b2ic41p";
+    feedsSha256.base = "0bgfrgw92852j6yzbblkyw7w7j8rqhmnk844y2b7l8400m9ncci2";
+  };
+  "mvebu"."cortexa72" = {
+    sha256 = "17d850m6ycwgkjflq3wrhyhlsjcpqp0j3j7px9kmw7qw42bdvjrn";
+    feedsSha256.base = "13ll513j7basdlf4j0bqfsskf33jfvvmbhzhm0h8qq1cpnars597";
+  };
+  "mxs"."generic" = {
+    sha256 = "0qln1dqdm84yakiq1zndyqw57sq7lp9q0jdms5bfzg1w76p9brls";
+  };
+  "octeon"."generic" = {
+    sha256 = "1i5flja2574s52qpn9zaqf8j4xbdc8igmi1k1ym4w7s0kkcfb6bv";
+    feedsSha256.base = "0lkvrbalbwd9hiaapa20bbza4wnkhjj29ibyaaj29s8wp7y03290";
+  };
+  "octeontx"."generic" = {
+    sha256 = "0v0vw1xqg1d1yjd2a9dl51yrw085899nbs5ghrddxcw2ilwh86zq";
+  };
+  "omap"."generic" = {
+    sha256 = "0v2lgvsyck05r54gfa9lrb2vbgysc4vjsjgyqlpmhfb20iadjpw5";
+    feedsSha256.base = "0wk705r2b7m6ngs55n5wvbnsn88ks6567q3s1j91dw6q5pfyrb1p";
+  };
+  "oxnas"."ox820" = {
+    sha256 = "16kahlv05g6riivb8xx9wa9rpb1rc349rpbya0aphs7v6z5v44x1";
+    feedsSha256.base = "1sy1i1r4lhmpksa54sqlnxbwxzb6s5kwgb6nj5qwqn5w6mk19pf9";
+  };
+  "pistachio"."generic" = {
+    sha256 = "1hcbwrsndddyc9riwna43avcm2f9w61b12xqiid43d0snyhhkf99";
+    feedsSha256.base = "1k9sn5faya8qv8bv5qqfyn5v57sdww57w3dlhivcp9p8n88siha4";
+  };
+  "ramips"."mt7620" = {
+    sha256 = "1j4iysvkyy1rbizvhz4gjsrd0zwkskdfy52xx0k2119mbnx5hvzy";
+    feedsSha256.base = "048n4jfi03ccb3qpbv7y2xcd3yc4xg2jwrvvxsiw754kjbagfcnn";
+  };
+  "ramips"."mt7621" = {
+    sha256 = "1n6hifxyk8bgs1pwbnw2v82ccka4xmabfyqall4imvr7va846l49";
+    feedsSha256.base = "048n4jfi03ccb3qpbv7y2xcd3yc4xg2jwrvvxsiw754kjbagfcnn";
+  };
+  "ramips"."mt76x8" = {
+    sha256 = "0k44k2g1vkz65qqb7ad4z212k9xzx77jrzm22awbqbmvwp42k9ij";
+    feedsSha256.base = "048n4jfi03ccb3qpbv7y2xcd3yc4xg2jwrvvxsiw754kjbagfcnn";
+  };
+  "ramips"."rt288x" = {
+    sha256 = "0232nrklnf1yk55hg896qf8ryr03x7wjdjxr3byvf75ch9jhjs89";
+    feedsSha256.base = "048n4jfi03ccb3qpbv7y2xcd3yc4xg2jwrvvxsiw754kjbagfcnn";
+  };
+  "ramips"."rt305x" = {
+    sha256 = "1ijqhpd30d8nzizvdlk21zxiikiacclp8580zz4zq04nh0vhb21h";
+    feedsSha256.base = "048n4jfi03ccb3qpbv7y2xcd3yc4xg2jwrvvxsiw754kjbagfcnn";
+  };
+  "ramips"."rt3883" = {
+    sha256 = "0fj4r5309sg9qh1dwslrm41zah9skw604f5p3wbykmyfzn1qamx0";
+    feedsSha256.base = "1p31ig4r6frg7wn9z3dfsr3mz2hg6a8633k79nipcpf1jf060flg";
+  };
+  "realtek"."rtl838x" = {
+    sha256 = "01zy3cwv93kyzfjixij9mk4xkdjsd18h2ca6l35dj4h1haaf03m3";
+    feedsSha256.base = "0ingk4bmkwqjr3i3ks0yp36cdg1wdg8dsr068j74ynnyz11vp536";
+  };
+  "realtek"."rtl839x" = {
+    sha256 = "0cnc9azsv6nkngvm9z81vnwr6pnrk11sd3ipmmwb078c08wpzww3";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "realtek"."rtl930x" = {
+    sha256 = "1gjassa7cfcmkdlhh6aplbc2rgpqh0kfpxz85r2lla68ikslrwnv";
+    feedsSha256.base = "1v836nfmfvdm19cfzlazm1p9b2va7fdjnwfmv8yq7rzp2188z71i";
+  };
+  "realtek"."rtl931x" = {
+    sha256 = "1qa69c6ifymnch5dkkqgs8xgzg8arl0sd8xq6ydjyql6s923bsla";
+  };
+  "rockchip"."armv8" = {
+    sha256 = "05id8wyb5g0lakm9c7b71rifgl6wwzbm1x8pc4g1k5y0fmwjpbpz";
+    feedsSha256.base = "1h3ail2bphhvd8vddarqna1s968zxfviqn8q112hjn9cxcmbx6q5";
+  };
+  "sunxi"."cortexa8" = {
+    sha256 = "12psiwfp1vvx8b30149zmiw8ghhfmh4g6mqv5vpv20mcj6kp46xz";
+    feedsSha256.base = "0wk705r2b7m6ngs55n5wvbnsn88ks6567q3s1j91dw6q5pfyrb1p";
+  };
+  "sunxi"."cortexa7" = {
+    sha256 = "1bj0jasnl3cfc372jn57a3j7wgsnwja8fq2h48yymwnaq8cpqdla";
+    feedsSha256.base = "0nd4irbbq8jm8ivd2h1swhn7j22i211z8y6xm0ywa65n6pl9qc85";
+  };
+  "sunxi"."cortexa53" = {
+    sha256 = "0hrbrc5n6vij2p4slkizva9faaqh5m3i2ji073is4da4ylbp1hgg";
+    feedsSha256.base = "0bgfrgw92852j6yzbblkyw7w7j8rqhmnk844y2b7l8400m9ncci2";
+  };
+  "tegra"."generic" = {
+    sha256 = "05811iifkdrg2mcfc5q420khcgl95w7p34bpf7riyiq00v1ck6r2";
+    feedsSha256.base = "0za06nwg5i0xb810vk4plg97lfhb63k8707v58q36qg96zvhfkpc";
+  };
+  "x86"."generic" = {
+    sha256 = "00whp4s14bmvlc7l9jmsh6p1manqrlf9cyqv6cy9390f6kqd3qk4";
+    feedsSha256.base = "1zx4aqvxmvs78pdrlg2zm2913dk72lqzzr4kf1jjpk3301jpnp13";
+  };
+  "x86"."legacy" = {
+    sha256 = "1jbydifk9g9xv5k5gcf6smxsjvlrpgkvzakqa4xwb16n775ydfwz";
+    feedsSha256.base = "1pkjw331jgjz66ykg6mhpqwp5jplwjjkg46gxwajx80cmin6sms4";
+  };
+  "x86"."geode" = {
+    sha256 = "1say9v786wkq413w5ib0sn9bvwgvz47w2jrygn8vkg8b2ghibyzg";
+    feedsSha256.base = "1pkjw331jgjz66ykg6mhpqwp5jplwjjkg46gxwajx80cmin6sms4";
+  };
+  "x86"."64" = {
+    sha256 = "0z5x82aqccflic84kcyxafm01s7xb85m95hkdcklnavk11mi1w6d";
+    feedsSha256.base = "1vl1aicpbhlj83lgam2x5b0310vy5i1vyjla0ynh8fvqxad7jqk8";
+  };
+  "zynq"."generic" = {
+    sha256 = "1rszg5b8jv8slci0vsdfm7sx7k407wyzdwizsmvl2pamqa3a2s8w";
+    feedsSha256.base = "0wlh5gg014zyc1i52ydxj7g87hz9ijzvvk7aq0pamy8wird2pv2x";
+  };
+}
