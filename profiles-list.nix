bb757271e3cf3ea8c74cec45318c4e7ede94e1f4
Astro <astro@spaceboyz.net>
2022-04-28 03:12:41 +0200

    profiles-list: generate for all known releases

--- profiles-list.nix
+++ profiles-list.nix
@@ -1,21 +1,37 @@
 { pkgs ? import <nixpkgs> {}
-, release ? "21.02.3"
 }:
 with pkgs;
 let
-  inherit (import ./profiles.nix { inherit pkgs; }) allProfiles;
+  releases =
+    map (builtins.replaceStrings [ ".nix" ] [ "" ]) (
+      builtins.filter (lib.hasSuffix ".nix") (
+        builtins.attrNames (builtins.readDir ./hashes)
+      )
+    );
+
+  list = release:
+    let
+      inherit (import ./profiles.nix {
+        inherit pkgs release;
+      }) allProfiles;
+    in writeText "openwrt-${release}-profiles-list.md" ''
+      # OpenWRT ${release} profiles
+
+      ${lib.concatMapStrings (target:
+        lib.concatMapStrings (variant: ''
+          ## ${target}/${variant}
+
+          ${lib.concatMapStrings (profile: ''
+            - ${profile}
+          '') (builtins.attrNames allProfiles.${target}.${variant}.profiles)}
+
+        '') (builtins.attrNames allProfiles.${target})
+      ) (builtins.attrNames allProfiles)}
+    '';
 in
-writeText "openwrt-${release}-profiles-list.md" ''
-  # OpenWRT ${release} profiles
-
-  ${lib.concatMapStrings (target:
-    lib.concatMapStrings (variant: ''
-      ## ${target}/${variant}
-
-      ${lib.concatMapStrings (profile: ''
-        - ${profile}
-      '') (builtins.attrNames allProfiles.${target}.${variant}.profiles)}
-
-    '') (builtins.attrNames allProfiles.${target})
-  ) (builtins.attrNames allProfiles)}
+runCommandNoCC "openwrt-profiles" {} ''
+  mkdir $out
+  ${lib.concatMapStrings (release: ''
+    ln -s ${list release} $out/${release}.md
+  '') releases}
 ''
