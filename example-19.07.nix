fa9d83b13e93c7c8630d59e49e69243b8651bb8d
Astro <astro@spaceboyz.net>
2022-09-12 20:58:10 +0200

    example-19.07: add legacy example

---
+++ example-19.07.nix
@@ -0,0 +1,25 @@
+{ pkgs ? import <nixpkgs> {}
+, profiles ? import ./profiles.nix { inherit pkgs; }
+, build ? import ./builder.nix
+}:
+build {
+  release = "19.07.10";
+  target = "ar71xx";
+  variant = "tiny";
+  profile = "tl-wr841-v10";
+  packages = [
+    "tcpdump"
+    "vxlan" "kmod-vxlan"
+    "-kmod-pppoa"
+    "collectd" "collectd-mod-iwinfo" "collectd-mod-ping"
+  ];
+  files = pkgs.runCommandNoCC "image-files" {} ''
+    mkdir -p $out/etc/uci-defaults
+    cat > $out/etc/uci-defaults/99-custom <<EOF
+    uci -q batch << EOI
+    set system.@system[0].hostname='testap'
+    commit
+    EOI
+    EOF
+  '';
+}
