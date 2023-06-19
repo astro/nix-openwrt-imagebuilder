67ce3d103cb29c33e781d9fa219651799e7db09e
Astro <astro@spaceboyz.net>
2023-06-19 23:23:54 +0200

    generate-all-hashes: start ignoring -rc releases
    Fixes Github issue #30

--- generate-all-hashes.nix
+++ generate-all-hashes.nix
@@ -8,8 +8,9 @@ writeScriptBin "generate-all-hashes" ''
   PATH=${lib.makeBinPath [ self.packages.${system}.generate-hashes git ]}:$PATH
 
   RELEASES=$(curl -s https://downloads.openwrt.org |
-    grep -oP "OpenWrt [1-9][0-9\.\-a-zA-Z]+" |
-    sed -e 's/OpenWrt //'
+    grep -oP "OpenWrt [1-9][0-9\.]+<" |
+    sed -e 's/OpenWrt //' -e 's/<//' |
+    uniq
   )
   for RELEASE in $RELEASES snapshot ; do
     echo "# Fetching hashes for OpenWrt $RELEASE"
