80dc40757243ca609761203af130f4a520aa420a
Astro <astro@spaceboyz.net>
2024-01-31 18:06:52 +0100

    generate-all-hashes: update to new downloads.openwrt.org site

--- generate-all-hashes.nix
+++ generate-all-hashes.nix
@@ -5,9 +5,9 @@
 pkgs.writeShellScriptBin "generate-all-hashes" ''
   PATH=${pkgs.lib.makeBinPath (with pkgs; [ self.packages.${pkgs.system}.generate-hashes git ])}:$PATH
 
-  RELEASES=$(curl -s https://downloads.openwrt.org |
-    grep -oP "OpenWrt [1-9][0-9\.]+<" |
-    sed -e 's/OpenWrt //' -e 's/<//' |
+  RELEASES=$(curl -s https://downloads.openwrt.org/releases/ |
+    grep -oP "\"[1-9][0-9\.]+/\"" |
+    sed -e 's/"//g' -e 's/\///' |
     uniq
   )
   for RELEASE in $RELEASES snapshot ; do
