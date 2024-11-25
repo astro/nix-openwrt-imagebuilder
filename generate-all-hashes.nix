d4e5622f384f111e661758c281d87f483224e71e
Nikita Pedorich <pedorich.n@gmail.com>
2024-11-25 22:57:19 +0900

    Improve generate-all-hashes

--- generate-all-hashes.nix
+++ generate-all-hashes.nix
@@ -1,17 +1,22 @@
 { self
 , pkgs
 }:
+pkgs.writeShellApplication {
+  name = "generate-all-hashes";
+  runtimeInputs = [
+    pkgs.curl
+    pkgs.jq
+    self.packages.${pkgs.system}.generate-hashes
+  ];
 
-pkgs.writeShellScriptBin "generate-all-hashes" ''
-  PATH=${pkgs.lib.makeBinPath (with pkgs; [ self.packages.${pkgs.system}.generate-hashes git ])}:$PATH
+  text = ''
+    VERSIONS=$(curl -s https://downloads.openwrt.org/.versions.json)
+    readarray -t RELEASES < <(jq -r '.versions_list[]' <<< "''${VERSIONS}")
+    RELEASES+=("snapshot")
 
-  RELEASES=$(curl -s https://downloads.openwrt.org/releases/ |
-    grep -oP "\"[1-9][0-9\.]+/\"" |
-    sed -e 's/"//g' -e 's/\///' |
-    uniq
-  )
-  for RELEASE in $RELEASES snapshot ; do
-    echo "# Fetching hashes for OpenWrt $RELEASE"
-    generate-hashes $RELEASE
-  done
-''
+    for RELEASE in "''${RELEASES[@]}"; do
+      echo "# Fetching hashes for OpenWrt ''${RELEASE}"
+      generate-hashes "''${RELEASE}"
+    done
+  '';
+}
