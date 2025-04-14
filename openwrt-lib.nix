12de9d6ab4d07247330a3609611edaadcc07f4a0
Rodney Lorrimar <dev@rodney.id.au>
2025-04-14 16:40:52 +0800

    Make openwrt-lib extensible

--- openwrt-lib.nix
+++ openwrt-lib.nix
@@ -1,36 +1,35 @@
-{ lib ? import <nixpkgs/lib> }: let
-  openwrtLib = {
-    latestReleasePathRel = "cache/LATEST_RELEASE";
-    latestRelease = let
-      path = ./. + "/${openwrtLib.latestReleasePathRel}";
-    in
-      if builtins.pathExists path
-      then lib.trim (builtins.readFile path)
-      else throw "File ${openwrtLib.latestReleasePathRel} is missing";
+{ lib ? import <nixpkgs/lib> }:
 
-    compareReleases = v1: v2:
-      if v1 == v2 then 0
-      else if v1 == "snapshot" then 1
-      else if v2 == "snapshot" then -1
-      else lib.strings.compareVersions v1 v2;
-    releaseOlder = v1: v2: openwrtLib.compareReleases v2 v1 == 1;
-    releaseAtLeast = v1: v2: !openwrtLib.releaseOlder v1 v2;
+lib.makeExtensible (openwrtLib: {
+  latestReleasePathRel = "cache/LATEST_RELEASE";
+  latestRelease = let
+    path = ./. + "/${openwrtLib.latestReleasePathRel}";
+  in
+    if builtins.pathExists path
+    then lib.trim (builtins.readFile path)
+    else throw "File ${openwrtLib.latestReleasePathRel} is missing";
 
-    releases = import ./releases.nix { inherit lib; };
+  compareReleases = v1: v2:
+    if v1 == v2 then 0
+    else if v1 == "snapshot" then 1
+    else if v2 == "snapshot" then -1
+    else lib.strings.compareVersions v1 v2;
+  releaseOlder = v1: v2: openwrtLib.compareReleases v2 v1 == 1;
+  releaseAtLeast = v1: v2: !openwrtLib.releaseOlder v1 v2;
 
-    getCachedRelease = release: let
-      cachePath = ./cache/${release}/default.nix;
-    in
-      if builtins.pathExists cachePath
-      then import cachePath
-      else throw "Release \"${release}\" is not cached here.";
+  releases = import ./releases.nix { inherit lib; };
 
-    sanitizeFilename = fileName:
-      builtins.replaceStrings [ "~" ] [ "-" ] (
-        builtins.baseNameOf fileName
-      );
+  getCachedRelease = release: let
+    cachePath = ./cache/${release}/default.nix;
+  in
+    if builtins.pathExists cachePath
+    then import cachePath
+    else throw "Release \"${release}\" is not cached here.";
 
-    takeAttrs = attrNames: lib.intersectAttrs (lib.genAttrs attrNames lib.id);
-  };
-in
-  openwrtLib
+  sanitizeFilename = fileName:
+    builtins.replaceStrings [ "~" ] [ "-" ] (
+      builtins.baseNameOf fileName
+    );
+
+  takeAttrs = attrNames: lib.intersectAttrs (lib.genAttrs attrNames lib.id);
+})
