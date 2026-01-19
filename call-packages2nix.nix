87229308b3334edca0e682d892e78fa06d9b58d4
Astro <astro@spaceboyz.net>
2026-01-19 03:02:10 +0100

    call-packages2nix.nix: replace obsolete runCommandNoCC

--- call-packages2nix.nix
+++ call-packages2nix.nix
@@ -1,10 +1,10 @@
 { lib
-, runCommandNoCC
+, runCommand
 , packages2nix
 }:
 
 { mode, packages, sha256sums, prefix }: let
-  drv = runCommandNoCC "${packages.name}-ifd" {} ''
+  drv = runCommand "${packages.name}-ifd" {} ''
     ${packages2nix}/bin/packages2nix ${mode} ${packages} ${sha256sums} ${prefix} > $out
   '';
 in
